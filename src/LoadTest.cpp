d8ca4ac543ec5a6a1095e043d027546676957007
Philipp Basler <28863303+phbasler@users.noreply.github.com>
2025-05-01 07:58:49 +0200

    add loadtest

---
+++ src/LoadTest.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <dlfcn.h>
+
+int main(int argc, char *argv[])
+{
+    auto library = dlopen(LibPath, RTLD_NOW);
+
+    std::cout << "Library " << LibPath << " was ";
+    if(library == nullptr)
+    {
+        std::cout << "not ";
+    }
+    std::cout << "loaded" << std::endl;
+
+    std::cout << dlclose(library) << std::endl;
+}
